1ff44d17dd4ed20dd8ab21b7e0aca44c8fe29809
sanictaels <45869889+sanictaels@users.noreply.github.com>
2023-05-26 11:21:22 +0800

    Update alive.py
    added headless firefox to take screenshots of websites that are alive

--- alive.py
+++ alive.py
@@ -1,8 +1,17 @@
 import requests
 import sys
 from datetime import datetime
+from time import sleep
+from selenium import webdriver
 
 def main():
+    
+    #define webbrowser driver
+    #define firefox headless
+    option = webdriver.FirefoxOptions()
+    option.add_argument("--headless")
+    driver=webdriver.Firefox(options=option)
+    
     # Get the input file name from the user.
     input_file_name = input("Enter the input file name: ")
 #    input_file_name = "short_dns.txt"
@@ -22,10 +31,16 @@ def main():
             print("URL is http://" + domain_name + " " + str(response))
             if response.status_code :
                 up_domains.append(domain_name)
+                driver.get("http://{}/".format(domain_name))
+                sleep(1)
+                driver.get_screenshot_as_file("http_"+domain_name+".png")
             response = requests.get("https://{}/".format(domain_name),timeout=5)
             print("URL is https://" + domain_name + " " + str(response))
             if response.status_code :
                 up_domains.append(domain_name)
+                driver.get("https://{}/".format(domain_name))
+                sleep(1)
+                driver.get_screenshot_as_file("https_"+domain_name+".png")
         except requests.exceptions.RequestException:
             print(domain_name + " is ded")
             pass
